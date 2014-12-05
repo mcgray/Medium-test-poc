99267e6bf08babf7f85006def401a0abe3c0d957
Oleksiy Rezchykov aka mcgray <alex.mcgray@gmail.com>
2014-12-06 00:44:17 +0200

    Large tests

--- integration-tests/src/test/groovy/ua/com/mcgray/ToDoServiceTest.groovy
+++ integration-tests/src/test/groovy/ua/com/mcgray/ToDoServiceTest.groovy
@@ -1,22 +1,33 @@
 package ua.com.mcgray
-
+import com.jayway.jsonpath.JsonPath
+import org.springframework.http.HttpMethod
+import spock.lang.Shared
 import spock.lang.Specification
-import ua.com.mcgray.test.LargeTest
-
+import ua.com.mcgray.utils.RestCaller
 /**
  * @author orezchykov
  * @since 05.12.14
  */
 
-@Category(LargeTest)
 class ToDoServiceTest extends Specification {
 
+    @Shared
+    def restCaller = new RestCaller(8800, "todoService")
 
-    def 'should get a list of ToDos by user Id using a Hessian call'() {
-        given:
-        when:
-        then:
-        assert false
+    def "should get a list of ToDos by user Id using a Hessian call"() {
+        given: "user id equals 1"
+            def userId = 1
+
+        when:  "when I call ToDO service"
+
+            def result = restCaller.queryApi(HttpMethod.GET, "/remoting/api/todo/" + userId)
+
+        then: "then I get the ToDo list for the user"
+        assert result
+        def readContext = JsonPath.parse(result.getBody())
+
+        assert readContext.read("\$.[0].title") == "User personal ToDo"
+
     }
 
 
