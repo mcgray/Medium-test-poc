8fcc25c5e3776ddea7b7e460a7fb6d6141a16ae6
Oleksiy Rezchykov aka mcgray <alex.mcgray@gmail.com>
2015-02-12 16:31:03 +0200

    Externalized Boot, log and application configuration.

--- user-service-common/src/main/java/ua/com/mcgray/configuration/SImpleFileConfigurationService.java
+++ user-service-common/src/main/java/ua/com/mcgray/configuration/SImpleFileConfigurationService.java
@@ -1,12 +1,15 @@
 package ua.com.mcgray.configuration;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
 
 /**
  * @author orezchykov
@@ -15,11 +18,26 @@ import org.springframework.core.env.Environment;
 
 @Configuration
 //@PropertySource("file:/etc/mcgray/configuration.properties")
-@PropertySource("classpath:configuration.properties")
+//@PropertySource("classpath:configuration.properties")
 public class SimpleFileConfigurationService implements ConfigurationService {
 
+    @Value("${config.location}")
+    private String configLocation;
+
     @Autowired
-    private Environment environment;
+    private ResourceLoader resourceLoader;
+
+    private Properties properties;
+
+    @PostConstruct
+    protected void loadProperties() {
+        try {
+            properties = new Properties();
+            properties.load(resourceLoader.getResource(configLocation).getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't find services.properties", e);
+        }
+    }
 
 
     @Override
@@ -27,17 +45,17 @@ public class SimpleFileConfigurationService implements ConfigurationService {
 
         DbConfiguration dbConfiguration = new DbConfiguration();
 
-        dbConfiguration.setDriverClassName(environment.getProperty("db.general.driverclassname"));
-        dbConfiguration.setMaxActive(Integer.parseInt(environment.getProperty("db.general.maxactive")));
-        dbConfiguration.setMinIdle(Integer.parseInt(environment.getProperty("db.general.minidle")));
-        dbConfiguration.setMaxIdle(Integer.parseInt(environment.getProperty("db.general.maxidle")));
-        dbConfiguration.setMaxWait(Integer.parseInt(environment.getProperty("db.general.maxwait")));
-        dbConfiguration.setValidationQuery(environment.getProperty("db.general.validationquery"));
-        dbConfiguration.setTestOnBorrow(Boolean.parseBoolean(environment.getProperty("db.general.testonborrow")));
-        dbConfiguration.setTestWhileIdle(Boolean.parseBoolean(environment.getProperty("db.general.testwhileidle")));
-        dbConfiguration.setUrl(environment.getProperty("db.user.url"));
-        dbConfiguration.setUser(environment.getProperty("db.user.user"));
-        dbConfiguration.setPassword(environment.getProperty("db.user.password"));
+        dbConfiguration.setDriverClassName(properties.getProperty("db.general.driverclassname"));
+        dbConfiguration.setMaxActive(Integer.parseInt(properties.getProperty("db.general.maxactive")));
+        dbConfiguration.setMinIdle(Integer.parseInt(properties.getProperty("db.general.minidle")));
+        dbConfiguration.setMaxIdle(Integer.parseInt(properties.getProperty("db.general.maxidle")));
+        dbConfiguration.setMaxWait(Integer.parseInt(properties.getProperty("db.general.maxwait")));
+        dbConfiguration.setValidationQuery(properties.getProperty("db.general.validationquery"));
+        dbConfiguration.setTestOnBorrow(Boolean.parseBoolean(properties.getProperty("db.general.testonborrow")));
+        dbConfiguration.setTestWhileIdle(Boolean.parseBoolean(properties.getProperty("db.general.testwhileidle")));
+        dbConfiguration.setUrl(properties.getProperty("db.user.url"));
+        dbConfiguration.setUser(properties.getProperty("db.user.user"));
+        dbConfiguration.setPassword(properties.getProperty("db.user.password"));
         
         return dbConfiguration;
         
@@ -45,27 +63,27 @@ public class SimpleFileConfigurationService implements ConfigurationService {
 
     @Override
     public List<String> getCouchbaseHosts() {
-        return Arrays.asList(environment.getProperty("couchbase.todo.hosts").split(","));
+        return Arrays.asList(properties.getProperty("couchbase.todo.hosts").split(","));
     }
 
     @Override
     public String getCouchbaseBucketName() {
-        return environment.getProperty("couchbase.todo.bucket");
+        return properties.getProperty("couchbase.todo.bucket");
     }
 
     @Override
     public String getCouchbaseBucketPassword() {
-        return environment.getProperty("couchbase.todo.password");
+        return properties.getProperty("couchbase.todo.password");
     }
 
     @Override
     public String getUserServiceUrl() {
-        return environment.getProperty("service.user.url");
+        return properties.getProperty("service.user.url");
     }
 
     @Override
     public String getAccountServiceUrl() {
-        return environment.getProperty("service.account.url");
+        return properties.getProperty("service.account.url");
     }
 
 }
